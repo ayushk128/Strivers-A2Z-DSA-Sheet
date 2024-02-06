3d92097776a50af5b6c39ea1bf00c2718fa5939f
ayushk128 <ayushk128@gmail.com>
2024-02-06 19:46:44 +0530

    Added Minimum subset sum differnect and partially completed count subsets with sum equal to K

--- Step 16 Dynamic Programming/Step 16.4 DP on Subsequences/3MinimumSubsetSumDifference.cpp
+++ Step 16 Dynamic Programming/Step 16.4 DP on Subsequences/3MinimumSubsetSumDifference.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool partitionEqualTabulation(vector<int> &arr, int sum) {
+int minimumSubsetSumDifference(vector<int> &arr, int sum) {
     int n = arr.size();
-    vector<vector<bool>> dp(n, vector<bool>(target + 1, 0));
+    vector<vector<bool>> dp(n, vector<bool>(sum + 1, 0));
 
     for(int i = 0; i<n; i++) {
         dp[i][0] = 0;
     }
 
-    if(arr[0] <= target)
+    if(arr[0] <= sum)
         dp[0][arr[0]] = true;
 
     for(int ind = 1; ind < n; ind++) {
-        for(int tar = 1; tar <= target; tar++) {
+        for(int tar = 1; tar <= sum; tar++) {
             bool notTake = dp[ind - 1][tar];
             bool take = false;
             if(arr[ind] <= tar) take = dp[ind - 1][tar - arr[ind]];
@@ -21,13 +21,19 @@ bool partitionEqualTabulation(vector<int> &arr, int sum) {
             dp[ind][tar] = notTake || take;
         }
     }
-    return dp[n - 1][target];
+    int ans = INT_MAX;
+    for(int i = 0; i<=sum; i++) {
+        if(dp[n - 1][i] == true) {
+            int difference = abs(i - (sum - i));
+            ans = min(ans, difference);
+        }
+    }
+    return ans;
 }
 
-void helper(int sum, vector<int> &arr) {
+int helper(int sum, vector<int> &arr) {
     int n = arr.size();
-    int sum = accumulate(arr.begin(), arr.end());
-    
+    return minimumSubsetSumDifference(arr, sum);
 }
 int main() {
     freopen("input.txt", "r", stdin);
@@ -43,5 +49,5 @@ int main() {
 
     int sum = accumulate(arr.begin(), arr.end(), 0);
 
-    helper(sum, arr);
+    cout << helper(sum, arr) << endl;
 }
---
+++ Step 16 Dynamic Programming/Step 16.4 DP on Subsequences/4CountSubsetsWithSumK.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+int numberOfSubsets(int ind, int sum, vector<int> arr) {
+    if(ind == 0) {
+        if(sum == 0 && arr[ind] == 0) return 2;
+        if(sum == 0 || arr[ind] == sum) return 1;
+        return 0;
+    }
+
+    int notTake = numberOfSubsets(ind - 1, sum, arr);
+    int take = 0;
+    if(arr[ind] <= sum) take = numberOfSubsets(ind - 1, sum - arr[ind], arr);
+
+    return notTake + take;
+}
+
+int numberOfSubsetsMemoization(int ind, int sum, vector<int> arr, vector<vector<int>> &dp) {
+    if(ind == 0) {
+        if(sum == 0 && arr[ind] == 0) return 2;
+        if(sum == 0 || arr[ind] == sum) return 1;
+        return 0;
+    }
+
+    if(dp[ind][sum] != -1) return dp[ind][sum];
+
+    int notTake = numberOfSubsetsMemoization(ind - 1, sum, arr, dp);
+    int take = 0;
+    if(arr[ind] <= sum) take = numberOfSubsetsMemoization(ind - 1, sum - arr[ind], arr, dp);
+
+    return dp[ind][sum] = notTake + take;
+}
+
+// int numberOfSubsetsTabulation(int sum, vector<int> arr) {
+//     int n = arr.size();
+//     vector<vector<int>> dp(n, vector<int>(sum + 1, 0));
+
+//     for(int i = 0; i<n; i++) {
+//         dp[i][0] = 1;
+//     }
+// }
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    int k;
+    cin >> k;
+
+    cout << numberOfSubsets(n - 1, k, arr) << endl;
+
+    vector<vector<int>> dp(n, vector<int>(k + 1, -1));
+    cout << numberOfSubsetsMemoization(n - 1, k, arr, dp) << endl;
+
+
+}
